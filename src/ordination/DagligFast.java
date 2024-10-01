203ef4c896c662a4cd88c4a7926e4d2d9e9c7a02
rasmusmeinert <77355900+rasmusmeinert@users.noreply.github.com>
2024-10-01 10:36:14 +0200

    DagligFast og Patientrettelse

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -1,5 +1,36 @@
 package ordination;
 
-public class DagligFast {
+import java.time.LocalDate;
 
+public class DagligFast extends Ordination {
+    private final Dosis[] doser = new Dosis[4];
+
+
+    public DagligFast(LocalDate startDato, LocalDate slutDato, Lægemiddel lægemiddel) {
+        super(startDato, slutDato, lægemiddel);
+    }
+
+    public void setDosis(int i, Dosis dosis) {
+        doser[i] = dosis;
+    }
+
+    @Override
+    public double samletDosis() {
+        return antalDage() * døgnDosis();
+    }
+
+
+    @Override
+    public double døgnDosis() {
+        double døgnDosis = 0;
+        for (int i = 0; i < 4; i++) {
+            døgnDosis = doser[i].getAntal();
+        }
+        return døgnDosis;
+    }
+
+    @Override
+    public String getType() {
+        return "DagligFast";
+    }
 }
