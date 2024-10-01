3f2d8da236ae960d824f406cddb10f4a1c68988e
Mads D <42411664+madshd@users.noreply.github.com>
2024-10-01 10:44:32 +0200

    DagligSkæv færdig

--- src/ordination/DagligSkæv.java
+++ src/ordination/DagligSkæv.java
@@ -5,15 +5,6 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
-/*
-Ved daglig skæv forstås, at medicinen gives dagligt, i den periode ordinationen er gyldig. Med skæv menes,
-at man ved ordinations oprettelse bestemmer, hvilke klokkeslæt medicinen skal gives på. Når ordinationen
-oprettes, angives det, hvor meget og på hvilke tidspunkter af dagen patienten skal have medicinen.
-Her kan angives et vilkårligt antal tidspunkter på døgnet, og for hvert tidspunkt angives antallet af enheder, patienten
-skal have af den pågældende medicin.
-
- • Kompositionen mellem DagligSkæv og Dosis skal realiseres ved en List<Dosis>
- */
 public class DagligSkæv extends Ordination {
     // Instance fields
     private final List<Dosis> doser = new ArrayList<Dosis>();
