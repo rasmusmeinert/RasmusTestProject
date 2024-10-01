8cd1119171ceb6cc297f9685b0d1dfbaf12a5fd1
jonathanhj123 <57926140+jonathanhj123@users.noreply.github.com>
2024-10-01 09:38:44 +0200

    Ordinationer i patient gangstababy

--- src/ordination/Patient.java
+++ src/ordination/Patient.java
@@ -1,6 +1,11 @@
 package ordination;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Patient {
+
+    private final ArrayList<Ordination> ordninationer = new ArrayList<Ordination>();
     private final String cprNr;
     private final String navn;
     private final double vægt;
