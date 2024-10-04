0e1c9f5f53b1d329d9fc109e1bc1e53971d947bf
rasmusmeinert <77355900+rasmusmeinert@users.noreply.github.com>
2024-10-04 09:14:05 +0200

    DagligTestTests

--- src/test/ordination/DagligFastTest.java
+++ src/test/ordination/DagligFastTest.java
@@ -1,7 +1,13 @@
 package test.ordination;
 
+import ordination.DagligFast;
+import ordination.Dosis;
+import ordination.Lægemiddel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,10 +18,50 @@ class DagligFastTest {
     }
 
     @Test
-    void samletDosis() {
+    void samletDosisTc1() {
+        Lægemiddel paracetamol = new Lægemiddel("Paracetamol", 1, 1.5, 2, "Ml");
+        DagligFast dagligFast = new DagligFast(LocalDate.of(2024, 12, 10), LocalDate.of(2024, 12, 20), paracetamol);
+        dagligFast.opretDosis(0, LocalTime.of(8, 0), 5);
+        dagligFast.opretDosis(1, LocalTime.of(12, 0), 5);
+        dagligFast.opretDosis(2, LocalTime.of(17, 0), 5);
+        dagligFast.opretDosis(3, LocalTime.of(23, 0), 5);
+
+        double samletDosis = dagligFast.samletDosis();
+        assertEquals(220,samletDosis);
+
+    }
+
+
+    @Test
+    void samletDosisTc2() {
+        Lægemiddel paracetamol = new Lægemiddel("Paracetamol", 1, 1.5, 2, "Ml");
+        DagligFast dagligFast = new DagligFast(LocalDate.of(2024, 12, 10), LocalDate.of(2024, 12, 20), paracetamol);
+
+        double samletDosis = dagligFast.samletDosis();
+        assertEquals(0,samletDosis);
+
     }
 
     @Test
-    void døgnDosis() {
+    void døgnDosisTc1() {
+        Lægemiddel paracetamol = new Lægemiddel("Paracetamol", 1, 1.5, 2, "Ml");
+        DagligFast dagligFast = new DagligFast(LocalDate.of(2024, 12, 10), LocalDate.of(2024, 12, 20), paracetamol);
+        dagligFast.opretDosis(0, LocalTime.of(8, 0), 5);
+        dagligFast.opretDosis(1, LocalTime.of(12, 0), 5);
+        dagligFast.opretDosis(2, LocalTime.of(17, 0), 5);
+        dagligFast.opretDosis(3, LocalTime.of(23, 0), 5);
+
+        double døgnDosis = dagligFast.døgnDosis();
+        assertEquals(20,døgnDosis);
+    }
+
+
+    @Test
+    void døgnDosisTc2() {
+        Lægemiddel paracetamol = new Lægemiddel("Paracetamol", 1, 1.5, 2, "Ml");
+        DagligFast dagligFast = new DagligFast(LocalDate.of(2024, 12, 10), LocalDate.of(2024, 12, 20), paracetamol);
+
+        double døgnDosis = dagligFast.døgnDosis();
+        assertEquals(0,døgnDosis);
     }
 }
